ad0c793325660952171aab7072c86b0ee1d5789e
LoganTunes <134849109+LoganTunes@users.noreply.github.com>
2026-09-07 12:53:51 -0500

    Add proper imports to games/__init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,1 +1,20 @@
 import asyncio
+from . import blackjack
+from . import craps
+from . import keno
+from . import mechanical_derby
+from . import roulette
+from . import slots
+from . import ultimate_hold_em
+from . import video_poker
+
+__all__ = [
+    "blackjack",
+    "craps",
+    "keno",
+    "mechanical_derby",
+    "roulette",
+    "slots",
+    "ultimate_hold_em",
+    "video_poker",
+]
