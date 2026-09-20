4113275942b05371b21053a1c5e1b2e466f4233b
LoganTunes <134849109+LoganTunes@users.noreply.github.com>
2026-09-20 11:04:22 -0500

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,18 +1,26 @@
 import asyncio
+from . import baccarat
+from . import big_six
 from . import blackjack
 from . import craps
 from . import keno
+from . import lottery
 from . import mechanical_derby
+from . import plinko
 from . import roulette
 from . import slots
 from . import ultimate_hold_em
 from . import video_poker
 
 __all__ = [
+    "baccarat",
+    "big_six",
     "blackjack",
     "craps",
     "keno",
+    "lottery",
     "mechanical_derby",
+    "plinko",
     "roulette",
     "slots",
     "ultimate_hold_em",
